1af8bb1c68cc9d1c02b62b814a1d4c2143e6b0c4
Tamado Sitohang <ramot@ramottamado.dev>
2022-02-09 02:05:46 +0700

    Add media support

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,12 +1,12 @@
-document.addEventListener('readystatechange', () => {
-  let nav = document.querySelector('nav');
-  let trig = document.querySelector('div.trigger');
+document.addEventListener("readystatechange", () => {
+  let nav = document.querySelector("nav");
+  let trig = document.querySelector("div.trigger");
 
   if (trig != null && isOverflown(trig, nav)) {
-    nav.classList.add('overflown');
+    nav.classList.add("overflown");
   }
 });
 
 function isOverflown(element, from) {
-  return element.offsetWidth >= (0.8 * from.clientWidth);
+  return element.offsetWidth >= 0.8 * from.clientWidth;
 }
